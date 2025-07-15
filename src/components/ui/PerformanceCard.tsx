0a09821a101a945d9f1c509efe56b78f7dc3534e
Geunwon Park <rmsdnjsaos@gmail.com>
2025-07-15 22:15:46 +0900

    공연 목록 조회 단위테스트

---
+++ src/components/ui/PerformanceCard.tsx
@@ -0,0 +1,36 @@
+type performanceData = {
+	id: number;
+	name: string;
+	image: string;
+	type: string;
+	genre: string;
+	start_date: string;
+	end_date: string;
+	stadium: string;
+	averageStar: number;
+};
+
+interface performanceCardProps {
+	performance: performanceData;
+	onClick: (id: number) => void;
+}
+
+export default function PerformanceCard({
+	performance,
+	onClick,
+}: performanceCardProps) {
+	return (
+		<button
+			type="button"
+			data-testid="performanceId"
+			onClick={() => onClick(performance.id)}>
+			<img src={performance.image} alt={`${performance.name} 포스터 이미지`} />
+			<h2>{performance.name}</h2>
+			<p>{performance.stadium}</p>
+			<p>{performance.averageStar}</p>
+			<p>
+				{performance.start_date} ~ {performance.end_date}
+			</p>
+		</button>
+	);
+}
